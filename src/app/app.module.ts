b3c760366d25a5fcde831906fd407f5143f03d99
BWD <bdrakese@gmail.com>
2019-08-24 00:23:57 -0600

    Working on base modal and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ModalBaseComponent } from './modals/modal-base/modal-base.component';
+import { ConfirmDialogComponent } from './modals/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalBaseComponent,
+    ConfirmDialogComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/modals/confirm-dialog/confirm-dialog.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line: component-selector
+    selector: 'confirm-dialog',
+    templateUrl: './confirm-dialog.component.html',
+    styleUrls: ['./confirm-dialog.component.scss']
+})
+export class ConfirmDialogComponent implements OnInit {
+    @Input() title: string;
+    @Output() clickCancel: EventEmitter<any> = new EventEmitter<any>();
+    @Output() clickConfirm: EventEmitter<any> = new EventEmitter<any>();
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    emitCancel() {
+        console.log("Cancel");
+        this.clickCancel.emit("cancel");
+    }
+
+    emitConfirm() {
+        console.log("Confirm");
+        this.clickConfirm.emit("confirm");
+    }
+}
