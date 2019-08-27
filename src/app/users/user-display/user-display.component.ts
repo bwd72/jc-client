d20b8bf9d78ad637455d0d29ee54207ef6cb96ef
BWD <bdrakese@gmail.com>
2019-08-26 20:52:53 -0600

    Utilizing live data from users.service for the users display component

--- src/app/users/user-display/user-display.component.ts
+++ src/app/users/user-display/user-display.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'user-display',
-  templateUrl: './user-display.component.html',
-  styleUrls: ['./user-display.component.scss']
+    selector: 'user-display',
+    templateUrl: './user-display.component.html',
+    styleUrls: ['./user-display.component.scss']
 })
 export class UserDisplayComponent implements OnInit {
+    @Input() users: any;
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
