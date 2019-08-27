f55f91fe5e2395efbe1415fa3d7dc1905211d083
BWD <bdrakese@gmail.com>
2019-08-26 20:52:22 -0600

    Adding the users service

---
+++ src/app/services/users.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class UsersService {
+
+    // TODO: Read these from .env file
+    usersHTTPHost = "localhost:8005";
+    usersURL = `http://${this.usersHTTPHost}/api/systemusers`;
+
+    constructor(private http: HttpClient) { }
+
+    getUsers(): Observable<any> {
+        return this.http.get<any>(this.usersURL);
+    }
+}
