69b02f75eae6847aaa98f512885f9145fb136ae2
BWD <bdrakese@gmail.com>
2019-09-01 22:41:58 -0600

    Updating user-display for pop up dialog editor

--- src/app/users/user-display/user-display.component.ts
+++ src/app/users/user-display/user-display.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
     selector: 'user-display',
@@ -7,10 +8,21 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class UserDisplayComponent implements OnInit {
     @Input() users: any;
+    @Output() selectEvent: EventEmitter<any> = new EventEmitter<any>();
+    @Output() deleteEvent: EventEmitter<any> = new EventEmitter<any>();
 
     constructor() { }
 
     ngOnInit() {
     }
 
+
+    public emitSelect(event: MouseEvent, id: string) {
+        this.selectEvent.emit({ id, clientX: event.clientX, clientY: event.clientY });
+    }
+
+
+    public emitDelete(event: MouseEvent, id: string) {
+        this.deleteEvent.emit({ id, clientX: event.clientX, clientY: event.clientY });
+    }
 }
